086ac6b6858666775b711795e6577f2dd72d151d
Rian De Almeida Ferreira <64613463+Aerinho@users.noreply.github.com>
2022-05-15 19:41:24 -0300

    MovieCard

--- smr-dsmovie/dsmovie/frontend/src/App.tsx
+++ smr-dsmovie/dsmovie/frontend/src/App.tsx
@@ -1,10 +1,6 @@
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
-import Listing from 'pages/Listing';
-import Form from 'pages/Form';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Listing from "pages/Listing";
+import Form from "pages/Form";
 import Navbar from "components/Navbar";
 
 function App() {
